6aa6ba5a5bcbf8fda6ecbb3a5633d121d8e63db8
TheLDB <ldb@erikboles.com>
2022-09-18 20:45:43 -0600

    feat(feeds): updated feeds UI

--- degenslacker/components/Navbar.tsx
+++ degenslacker/components/Navbar.tsx
@@ -1,12 +1,14 @@
+import { useRouter } from "next/router";
 import ConnectedDesktopButtons from "./Navigation/ConnectedDesktopButtons";
 import DisconnectedDesktopButtons from "./Navigation/DisconnectedDesktopButtons";
 
 const Navbar = (props: { connected: boolean; env: string; }) => {
+    const router = useRouter();
     return (
         <div className="w-full h-full">
             {/* Desktop Navbar */}
             <div className="w-full h-full hidden sm:flex">
-                <div className="w-1/2 h-full p-4 flex items-center">
+                <div onClick={() => router.push("/")} className="w-1/2 h-full p-4 flex items-center hover:cursor-pointer">
                     <h1 className="font-sf-pro text-4xl text-white font-semibold">Degenslacker</h1>
                     <h1 className="font-sf-pro text-xl text-white font-semibold">.xyz</h1>
                 </div>
--- degenslacker/pages/feeds/[id].tsx
+++ degenslacker/pages/feeds/[id].tsx
@@ -2,9 +2,12 @@ import Head from "next/head";
 import { useState } from "react";
 import UserList from "../../components/Feeds/UserList";
 import Navbar from "../../components/Navbar";
+import { useRouter } from "next/router";
 
 const FeedID = () => {
     const [connected, setConnected] = useState(true);
+    const router = useRouter();
+    const { id } = router.query;
     const [users, setUsers] = useState<{
         userID: string;
         displayName: string;
@@ -70,11 +73,14 @@ const FeedID = () => {
     return (
         <div className="w-screen h-screen bg-gradient-to-br from-site-orange via-site-purple to-site-blue">
             <Head>
+                <title>
+                    DegenSlacker Feed
+                </title>
+            </Head>
+            <div className="w-full h-full bg-gradient-to-br from-site-orange via-site-purple to-site-blue flex justify-center items-center">
                 <div className="w-full h-24 absolute top-0 right-0 left-0">
                     <Navbar connected={connected} env={"feed"} />
                 </div>
-            </Head>
-            <div className="w-full h-full bg-gradient-to-br from-site-orange via-site-purple to-site-blue flex justify-center items-center">
             {connected ? <UserList users={users} />: <h1>asaaaa</h1>}
       </div>
         </div>
